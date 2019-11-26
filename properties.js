6380b9ed2e095d7fe6d5cae81ed6f7c99782e589
omkarBhoite <omkarBhoite@gmail.com>
2019-11-26 23:01:32 +0530

    new initial

---
+++ properties.js
@@ -0,0 +1,8 @@
+console.log("this is omkars program")
+console.log("space 2")
+
+
+console.log("this is omkars program")
+console.log("space 2")
+console.log("this is omkars program")
+console.log("space 2")
